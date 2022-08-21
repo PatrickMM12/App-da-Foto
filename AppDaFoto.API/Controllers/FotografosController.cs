0562f6bf56d1622418d5b030413af6e0ebef04c5
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-21 00:50:37 -0300

    Controller do Fotografo

---
+++ AppDaFoto.API/Controllers/FotografosController.cs
@@ -0,0 +1,65 @@
+﻿using app_da_foto.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using Repositorio;
+
+namespace FotografosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FotografosController : ControllerBase
+    {
+        private readonly IFotografoRepositorio _fotografoRepositorio;
+
+        public FotografosController()
+        {
+            this._fotografoRepositorio = new FotografoRepositorio();
+        }
+
+
+        [HttpGet]
+        [Route("todos/")]
+        public IEnumerable<Fotografo> GetFotografos()
+        {
+            return _fotografoRepositorio.BuscarTodosFotografos;
+        }
+
+        [HttpGet]
+        public IActionResult GetFotografo(string email, string senha)
+        {
+            Fotografo fotografo = _fotografoRepositorio.BuscarFotografo(email, senha);
+
+            if(fotografo == null)
+            {
+                return NotFound();
+            }
+            return new JsonResult(fotografo);
+        }
+
+        [HttpGet("{id}")]
+        public Fotografo GetFotografoPorId(int id)
+        {
+            return _fotografoRepositorio.BuscarFotografoPorId(id);
+        }
+
+        [HttpPost]
+        public IActionResult AddFotografo(Fotografo fotografo)
+        {
+            _fotografoRepositorio.AdicionarFotografo(fotografo);
+
+            return CreatedAtAction(nameof(GetFotografo), new { email = fotografo.Email, senha = fotografo.Senha }, fotografo);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Fotografo fotografo)
+        {
+            fotografo.Id = id;
+            _fotografoRepositorio.AtualizarFotografo(fotografo);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _fotografoRepositorio.DeletarFotografo(id);
+        }
+    }
+}
