d25bf395fdb90bd42033c3a1c57d088097ac5e83
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-26 20:43:21 -0300

    APP - Adição de Fontes

--- App-da-Foto/App-da-Foto/AssemblyInfo.cs
+++ App-da-Foto/App-da-Foto/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Regular.otf", Alias = "Montserrat")]
+[assembly: ExportFont("Montserrat-Light.otf", Alias = "MontserratLight")]
+[assembly: ExportFont("Montserrat-Bold.otf", Alias = "MontserratBold")]
