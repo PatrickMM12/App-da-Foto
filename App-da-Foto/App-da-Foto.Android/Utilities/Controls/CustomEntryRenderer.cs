179bb94368a89f072beee2f3b53f5a866350a45b
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-27 01:05:52 -0300

    APP - Remoção do Underline do Entry

---
+++ App-da-Foto/App-da-Foto.Android/Utilities/Controls/CustomEntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App_da_Foto.Droid.Utilities.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace App_da_Foto.Droid.Utilities.Controls
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.Background = null;
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
