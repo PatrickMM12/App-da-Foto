d4d98a3b3d90753b07382bc0a827227e81b86573
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-08-27 01:03:20 -0300

    APP - Configurações para Icone e Splash Screen no Android

--- App-da-Foto/App-da-Foto.Android/MainActivity.cs
+++ App-da-Foto/App-da-Foto.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Runtime;
 
 namespace App_da_Foto.Droid
 {
-    [Activity(Label = "App_da_Foto", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = false)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
