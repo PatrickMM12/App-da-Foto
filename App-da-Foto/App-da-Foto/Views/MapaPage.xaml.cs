75cb8a629d367b9a294b3a7c301521318fb6609d
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-09-05 21:27:45 -0300

    APP - Implementação do Mapa

---
+++ App-da-Foto/App-da-Foto/Views/MapaPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace App_da_Foto.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapaPage : ContentPage
+    {
+        public MapaPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //Map.GetActualLocationCommand.Execute(null);
+        }
+    }
+}
