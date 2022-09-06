953062afcc14d0e95d057360288cfa332170cd72
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-09-05 21:35:43 -0300

    APP - Classes para consumir a Web API Rest

---
+++ App-da-Foto/App-da-Foto/Services/FotografoService.cs
@@ -0,0 +1,68 @@
+﻿using app_da_foto.Domain.Model;
+using App_da_Foto.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_da_Foto.Services
+{
+    class FotografoService : Service
+    {
+        public async Task<ResponseService<Fotografo>> ObterFotografo(string email, string senha)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/Fotografo?email={email}&senha={senha}");
+
+            ResponseService<Fotografo> responseService = new ResponseService<Fotografo>();
+            responseService.IsSuccess = response.IsSuccessStatusCode;
+            responseService.StatusCode = (int)response.StatusCode;
+
+            if (response.IsSuccessStatusCode)
+            {
+                responseService.Data = await response.Content.ReadAsAsync<Fotografo>();
+            }
+            else
+            {
+                String problemResponse = await response.Content.ReadAsStringAsync();
+                var errors = JsonConvert.DeserializeObject<ResponseService<Fotografo>>(problemResponse);
+
+                responseService.Errors = errors.Errors;
+            }
+            return responseService;
+        }
+
+        public async Task<IEnumerable<Fotografo>> ObterFotografos()
+        {
+            HttpResponseMessage response = await _client.GetAsync($"{BaseApiUrl}/api/Fotografo/todos");
+
+            List<Fotografo> fotografos = null;
+            if (response.IsSuccessStatusCode)
+            {
+                fotografos = await response.Content.ReadAsAsync<List<Fotografo>>();
+            }
+
+            else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return fotografos;
+            }
+            return fotografos;
+        }
+
+        public async Task<Fotografo> AdicionarFotografo(Fotografo fotografo)
+        {
+            HttpResponseMessage response = await _client.PostAsJsonAsync($"{BaseApiUrl}/api/Fotografos", fotografo);
+
+            if (response.IsSuccessStatusCode)
+            {
+                fotografo = await response.Content.ReadAsAsync<Fotografo>();
+            }
+            else
+            {
+                fotografo = null;
+            }
+            return fotografo;
+        }
+    }
+}
---
+++ App-da-Foto/App-da-Foto/Services/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace App_da_Foto.Services
+{
+    class Service
+    {
+        protected HttpClient _client;
+        protected string BaseApiUrl = "https://appdafotoapi.azurewebsites.net/";
+
+        public Service()
+        {
+            _client = new HttpClient();
+        }
+    }
+}
